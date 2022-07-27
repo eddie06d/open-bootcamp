594a508f0051508ee08d4a1f7144a7c5ddcaaf6e
eddie06 <ejhuancahuire@gmail.com>
2022-07-27 18:19:32 -0500

    sintaxis, variables

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola, este es mi primer ejercicio con Node en el mejor Bootcamp de programación del mundo");
+let name = "Eddie Jamil";
+let age = 23;
+let isDeveloper = true;
+let birthDate = new Date("January 6, 1999");
+let favoriteBook = {
+    title: "Don Quijote de la Mancha",
+    author: "Miguel de Cervantes",
+    date: "1605",
+    url: "https://es.wikipedia.org/wiki/Don_Quijote_de_la_Mancha"
+};
+
+console.log(birthDate);
