fc4e0be0b1f891bec750a43a794855abd166bcff
eddie06 <ejhuancahuire@gmail.com>
2022-07-28 22:54:01 -0500

    numeros

---
+++ 05-numeros/index.js
@@ -0,0 +1,11 @@
+let alturaCm = 165;
+
+let alturaMt = 1.65;
+
+let pesoKg = 62.5;
+
+let redondeoUp = Math.ceil(alturaMt);
+
+let redondeoDown = Math.floor(pesoKg);
+
+let bool = Math.MAX_VALUE + 1 == Math.MAX_VALUE;
